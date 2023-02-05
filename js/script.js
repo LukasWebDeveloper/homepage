d97c3ed0471dc1ebdfc4d7135392dd6713fba301
Łukasz Duszak <lukas94.WebDeveloper@gmail.com>
2023-02-05 20:27:55 +0100

    all files modified

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const bodyElement = document.querySelector(".js-body");
+const headerElement = document.querySelector(".js-header");
+const buttonBackground = document.querySelector(".js-button--backgroundColor");
+const buttonSpanBackground = document.querySelector(".js-button__span--backgroundColor");
+
+buttonBackground.addEventListener("click", () => {
+    headerElement.classList.add("js-header");
+
+    if (buttonSpanBackground.classList.contains("js-header")){
+        buttonSpanBackground.innerText = "jasne";
+    }
+    else {
+        buttonSpanBackground.innerText = "ciemne";
+    }
+});
+buttonBackground.addEventListener("dblclick", () => {
+    headerElement.classList.remove("js-header");
+});
