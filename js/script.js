a1cc92f09512c8e622bd0e4efad503d089e12864
Łukasz Duszak <lukas94.WebDeveloper@gmail.com>
2023-02-05 20:44:29 +0100

    commenting elements

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@ const headerElement = document.querySelector(".js-header");
 const buttonBackground = document.querySelector(".js-button--backgroundColor");
 const buttonSpanBackground = document.querySelector(".js-button__span--backgroundColor");
 
-buttonBackground.addEventListener("click", () => {
-    headerElement.classList.add("js-header");
+// buttonBackground.addEventListener("click", () => {
+//     headerElement.classList.add("js-header");
 
-    if (buttonSpanBackground.classList.contains("js-header")){
-        buttonSpanBackground.innerText = "jasne";
-    }
-    else {
-        buttonSpanBackground.innerText = "ciemne";
-    }
-});
-buttonBackground.addEventListener("dblclick", () => {
-    headerElement.classList.remove("js-header");
-});
+//     if (buttonSpanBackground.classList.contains("js-header")){
+//         buttonSpanBackground.innerText = "jasne";
+//     }
+//     else {
+//         buttonSpanBackground.innerText = "ciemne";
+//     }
+// });
+// buttonBackground.addEventListener("dblclick", () => {
+//     headerElement.classList.remove("js-header");
+// });
